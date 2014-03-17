05e1e9356ac21db953a9c24bc5aa83396b4eb9a4
Alexey Ershov <alaershov@gmail.com>
2014-03-17 18:35:37 +0700

    Added tests for Token and Tokenizer

--- test/TokenTest.java
+++ test/TokenTest.java
@@ -1,13 +1,22 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TokenTest {
     @Test
     public void testGetType() throws Exception {
+        Token equals = new Token( Token.Type.EQUALS );
 
+        Assert.assertEquals( Token.Type.EQUALS, equals.getType() );
+
+        Token identifier = new Token( Token.Type.IDENTIFIER );
+
+        Assert.assertEquals( Token.Type.IDENTIFIER, identifier.getType() );
     }
 
     @Test
     public void testGetValue() throws Exception {
+        Token identifier = new Token( Token.Type.IDENTIFIER, "abc" );
 
+        Assert.assertEquals( "abc", identifier.getValue() );
     }
 }
--- test/TokenizerTest.java
+++ test/TokenizerTest.java
@@ -1,8 +1,27 @@
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.StringReader;
 
 public class TokenizerTest {
     @Test
     public void testGetToken() throws Exception {
+        BufferInterface buffer = new Buffer( new StringReader( "a = 775; /*comment\n \n * ** 31e1\n 127 == */ \n basket = 9798;" ), 10 );
 
+        TokenizerInterface tokenzer = new Tokenizer( buffer );
+        try {
+            Assert.assertEquals( Token.Type.IDENTIFIER, tokenzer.getToken().getType() );
+            Assert.assertEquals( Token.Type.ASSIGN, tokenzer.getToken().getType() );
+            Assert.assertEquals( Token.Type.INTEGER, tokenzer.getToken().getType() );
+            Assert.assertEquals( Token.Type.SEMICOLON, tokenzer.getToken().getType() );
+            Assert.assertEquals( Token.Type.IDENTIFIER, tokenzer.getToken().getType() );
+            Assert.assertEquals( Token.Type.ASSIGN, tokenzer.getToken().getType() );
+            Assert.assertEquals( Token.Type.INTEGER, tokenzer.getToken().getType() );
+            Assert.assertEquals( Token.Type.SEMICOLON, tokenzer.getToken().getType() );
+            Assert.assertEquals( Token.Type.END_OF_PROGRAM, tokenzer.getToken().getType() );
+
+        } catch ( IllegalCharacterException e ) {
+            System.err.println( e.getMessage() );
+        }
     }
 }
